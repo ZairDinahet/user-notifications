3fcc10602373fa4ac5ef9d7d290adf7e14bf21db
ZairDinahet <dinahetzair@gmail.com>
2025-10-28 22:58:15 -0300

    feat: create a User entity with properties and relationships for the notification system

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,41 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Notification } from '../../notifications/entities/notification.entity';
+import { RolesEnum, USER } from 'src/common/enums/roles.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: RolesEnum,
+    enumName: 'role',
+    default: RolesEnum.USER
+  })
+  role: RolesEnum;
+  
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamptz', nullable: true })
+  deletedAt?: Date | null;
+
+  @OneToMany(() => Notification, (notification) => notification.user)
+  notifications: Notification[];
+}
